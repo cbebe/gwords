820791e480a4ba80f8c4a56759ee38f1e5dcb742
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-06-10 17:56:21 -0600

    looks disgusting rn but it works

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 // TODO: handle non-Hangul strings and characters
 pub mod letter;
 
-pub fn gword(str: &str) -> String {
-    str.chars().fold(String::new(), |mut s, c| {
-        let [a, b] = letter::g_wordify(c);
-        s.push(a);
-        s.push(b);
-        s
+pub fn gword(phrase: &str) -> Result<String, &'static str> {
+    phrase.chars().try_fold(String::new(), |mut s, c| {
+        letter::g_wordify(c).and_then(|[a, b]| {
+            s.push(a);
+            s.push(b);
+            Ok(s)
+        })
     })
 }
 
@@ -15,7 +16,14 @@ mod test {
     use super::*;
     #[test]
     fn test_gword() {
-        assert_eq!("아간녀겅하가세게요고", gword("안녕하세요"));
-        assert_eq!("가감사가하갑니기다가", gword("감사합니다"));
+        assert_eq!(
+            Ok(String::from("아간녀겅하가세게요고")),
+            gword("안녕하세요")
+        );
+        assert_eq!(
+            Ok(String::from("가감사가하갑니기다가")),
+            gword("감사합니다")
+        );
+        assert_eq!(Err("Not Hangul"), gword("hello"));
     }
 }
