6922e6f69299cbc091cdbd05f87ad268815d3155
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-06-10 21:28:58 -0600

    return char if not valid

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,18 @@
 // TODO: handle non-Hangul strings and characters
 pub mod letter;
 
-pub fn gword(phrase: &str) -> Result<String, &'static str> {
-    phrase.chars().try_fold(String::new(), |mut s, c| {
-        letter::g_wordify(c).map(|[a, b]| {
-            s.push(a);
-            s.push(b);
-            s
-        })
-    })
+pub fn gword(phrase: &str) -> String {
+    let mut s = String::new();
+    for c in phrase.chars() {
+        match letter::g_wordify(c) {
+            Ok([a, b]) => {
+                s.push(a);
+                s.push(b);
+            }
+            Err(_) => s.push(c),
+        }
+    }
+    s
 }
 
 #[cfg(test)]
@@ -16,14 +20,9 @@ mod test {
     use super::*;
     #[test]
     fn test_gword() {
-        assert_eq!(
-            Ok(String::from("아간녀겅하가세게요고")),
-            gword("안녕하세요")
-        );
-        assert_eq!(
-            Ok(String::from("가감사가하갑니기다가")),
-            gword("감사합니다")
-        );
-        assert_eq!(Err("Not Hangul"), gword("hello"));
+        assert_eq!("아간녀겅하가세게요고", gword("안녕하세요"));
+        assert_eq!("아간녀겅하가세게요고!", gword("안녕하세요!"));
+        assert_eq!("가감사가하갑니기다가", gword("감사합니다"));
+        assert_eq!("hello", gword("hello"));
     }
 }
